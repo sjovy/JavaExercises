4ca8073b2870fd0befde6777a7c8e76e6aa3d9cc
sjovy <thomas.sjovy@gmail.com>
2024-02-28 15:20:54 +0100

    This is just a test commit I am sort of working on all exercises at once

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,12 +2,37 @@ package se.lexicon;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
+
+        String word1 = "Hello";
+        String word2 = "Thomas";
+        System.out.print(word1);
+        System.out.println(word2);
+
+        // Method to check if a year is a leap year
+        int year;
+        boolean isLeapYear = false;
+
+        year = 2020;
+        private static boolean isLeapYear(year) {
+            // Leap years are divisible by 4
+            // However, years divisible by 100 are not leap years unless they are also divisible by 400
+            if ( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )
+
+                ;
+        }
+
+
+        double num1, num2, num3;
+        num1 = 10;
+        num2 = 33;
+        num3 = num1 + num2;
+
+
+
+        // System.out.println(num3);
     }
 }
